34ffb3253ddb5d6222f6576a6f5f0fa8f81b6839
Oleksii <o.lytvynov@gastrofix.com>
2018-01-29 14:52:51 +0100

    Added folder

---
+++ JodelChallenge/FullviewScreen/FullImageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FullImageViewController.swift
+//  JodelChallenge
+//
+//  Created by Lesha on 28.01.2018.
+//  Copyright © 2018 Jodel. All rights reserved.
+//
+
+import UIKit
+
+class FullImageViewController: UIViewController {
+    
+    var imageToShow: UIImage?
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        self.imageView.image = self.imageToShow
+    }
+}
+
+extension FullImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView;
+    }
+}
